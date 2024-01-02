e3267a19dd94cf105fcf6dc6b5cbde1e416aac1f
Piruin Panichphol <piruin.p@gmail.com>
2024-01-02 12:26:50 +0700

    [IMP] ni_practitioner: auto add performer_id into encounter practitioner list

--- ni_practitioner/models/ni_encounter.py
+++ ni_practitioner/models/ni_encounter.py
@@ -1,6 +1,8 @@
 #  Copyright (c) 2023 NSTDA
 
+
 from odoo import api, fields, models
+from odoo.models import Command
 
 from odoo.addons.ni_patient.models.ni_encounter import LOCK_STATE_DICT
 
@@ -31,6 +33,55 @@ class Encounter(models.Model):
         states=LOCK_STATE_DICT,
         check_company=True,
     )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if "performer_id" in vals:
+                vals.update(
+                    self._prepare_participant_vals(
+                        vals["performer_id"], vals.get("period_start")
+                    )
+                )
+        return super(Encounter, self).create(vals_list)
+
+    def write(self, vals):
+        if "performer_id" not in vals:
+            return super(Encounter, self).write(vals)
+
+        vals.update(self._prepare_participant_vals(vals["performer_id"]))
+
+        enc = self.filtered_domain(
+            [
+                ("performer_id", "!=", vals["performer_id"]),
+            ]
+        )
+        prev_pperfomer_ids = enc.participant_ids.filtered_domain(
+            [
+                ("type_id", "=", self.env.ref("ni_patient.PPRF").id),
+                ("period_end", "=", False),
+            ]
+        )
+
+        result = super(Encounter, self).write(vals)
+        if result and prev_pperfomer_ids:
+            prev_pperfomer_ids.action_stop(self[0].write_date)
+
+        return result
+
+    @api.model
+    def _prepare_participant_vals(self, performer_id, start=None):
+        return {
+            "participant_ids": [
+                Command.create(
+                    {
+                        "employee_id": performer_id,
+                        "type_id": self.env.ref("ni_patient.PPRF").id,
+                        "period_start": start or fields.Datetime.now(),
+                    }
+                )
+            ]
+        }
 
     @api.onchange("performer_id")
     def onchange_performer_id(self):
