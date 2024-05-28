2a4fd3500bc57717d0e990f448949575e7603015
Piruin P. <piruin.p@gmail.com>
2024-05-28 13:30:37 +0700

    [IMP] ni_service: remove employee department color

--- ni_service/models/__init__.py
+++ ni_service/models/__init__.py
@@ -7,4 +7,3 @@ from . import ni_encounter
 from . import ni_encounter_class
 from . import ni_encounter_service_attendance
 from . import res_company
-from . import hr_employee
--- ni_service/models/hr_employee.py
+++
@@ -1,9 +0,0 @@
-#  Copyright (c) 2023 NSTDA
-
-from odoo import fields, models
-
-
-class Employee(models.Model):
-    _inherit = "hr.employee"
-
-    department_color = fields.Integer("Department Color", related="department_id.color")
