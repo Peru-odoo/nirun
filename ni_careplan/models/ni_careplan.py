361f4aa811580d1b2ab093e98174d9d03a913dcd
Pop Lacto <popliverpool1@gmail.com>
2024-09-15 18:35:29 +0700

    [IMP] ni_careplan: auto set condition on choose category & implement state lock

--- ni_careplan/models/ni_careplan.py
+++ ni_careplan/models/ni_careplan.py
@@ -3,6 +3,11 @@ from odoo import api, fields, models
 from odoo.fields import Command
 from odoo.tools.date_utils import relativedelta
 
+LOCK_STATE_DICT = {
+    "revoked": [("readonly", True)],
+    "completed": [("readonly", True)],
+}
+
 
 class Careplan(models.Model):
     _name = "ni.careplan"
@@ -17,22 +22,32 @@ class Careplan(models.Model):
         return res
 
     period_start = fields.Datetime(
-        default=lambda _: fields.Datetime.now().replace(day=1)
+        readonly=True,
+        states={"draft": [("readonly", False)]},
+        default=lambda _: fields.Datetime.now().replace(day=1),
     )
     period_end = fields.Datetime(
+        readonly=True,
+        states={"draft": [("readonly", False)]},
         default=lambda _: fields.Datetime.now()
-        + relativedelta(day=1, months=3, days=-1)
+        + relativedelta(day=1, months=3, days=-1),
     )
-
-    category_id = fields.Many2one("ni.careplan.category", required=True, index=True)
+    category_id = fields.Many2one(
+        "ni.careplan.category",
+        required=True,
+        index=True,
+        readonly=True,
+        states={"draft": [("readonly", False)]},
+    )
 
     condition_ids = fields.Many2many(
         "ni.condition",
         "ni_careplan_condition",
         "plan_id",
         "condition_id",
-        domain="[('patient_id', '=', patient_id)]",
+        domain="[('patient_id', '=', patient_id), ('clinical_state', '=', 'active')]",
         context={"default_patient_id": "patient_id"},
+        states=LOCK_STATE_DICT,
     )
     condition_count = fields.Integer(compute="_compute_condition_count")
     goal_mode = fields.Selection(
@@ -45,7 +60,10 @@ class Careplan(models.Model):
     goal_text = fields.Html()
     goal_category_id = fields.Many2one(related="category_id.goal_category_id")
     goal_ids = fields.One2many(
-        "ni.goal", "careplan_id", context={"default_patient_id": "patient_id"}
+        "ni.goal",
+        "careplan_id",
+        context={"default_patient_id": "patient_id"},
+        states=LOCK_STATE_DICT,
     )
     goal_review_ids = fields.One2many(related="goal_ids", string="Goal Review")
     goal_count = fields.Integer(compute="_compute_goal_ratio")
@@ -66,20 +84,40 @@ class Careplan(models.Model):
         "careplan_id",
         domain="[('category_id', '=?', service_category_id), ('intent', '=', 'plan')]",
         options="{'create': true}",
+        states=LOCK_STATE_DICT,
     )
     service_request_count = fields.Integer(compute="_compute_action_count")
-    medication_request_ids = fields.One2many("ni.medication.request", "careplan_id")
+    medication_request_ids = fields.One2many(
+        "ni.medication.request", "careplan_id", states=LOCK_STATE_DICT
+    )
     medication_request_count = fields.Integer(compute="_compute_action_count")
-    document_ids = fields.One2many("ni.document.ref", "careplan_id")
+    document_ids = fields.One2many(
+        "ni.document.ref", "careplan_id", states=LOCK_STATE_DICT
+    )
     document_count = fields.Integer(compute="_compute_document_count")
     achievement_id = fields.Many2one(
-        "ni.goal.achievement", domain=[("careplan", "=", True)]
+        "ni.goal.achievement", domain=[("careplan", "=", True)], states=LOCK_STATE_DICT
     )
     achievement_reason = fields.Html(help="Reason for current achievement")
     achievement_date = fields.Datetime(
         help="When achievement status took effect", readonly=1
     )
     achievement_uid = fields.Many2one("res.users", readonly=1)
+
+    @api.onchange("category_id")
+    def _onchange_category_id(self):
+        for rec in self:
+            if rec.category_id and rec.category_id.condition_code_ids:
+                condition = self.env["ni.condition"].search(
+                    [
+                        ("partner_id", "=", self.partner_id.ids[0]),
+                        ("code_id", "in", rec.category_id.condition_code_ids.ids),
+                        ("clinical_state", "=", "active"),
+                    ]
+                )
+                rec.condition_ids = condition
+            else:
+                rec.condition_ids = None
 
     @api.depends("condition_ids")
     def _compute_condition_count(self):
--- ni_careplan/models/ni_careplan_category.py
+++ ni_careplan/models/ni_careplan_category.py
@@ -7,5 +7,6 @@ class CareplanCategory(models.Model):
     _description = "Careplan Category"
     _inherit = ["ni.coding"]
 
+    condition_code_ids = fields.Many2many("ni.condition.code")
     goal_category_id = fields.Many2one("ni.goal.category")
     service_category_id = fields.Many2one("ni.service.category")
