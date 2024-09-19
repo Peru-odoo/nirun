727372040fecce1ae2dfd8847b471e5f2e89d89c
Piruin P. <piruin.p@gmail.com>
2024-09-19 14:56:45 +0700

    [IMP] ni_careplan: category form

--- ni_careplan/models/ni_careplan_category.py
+++ ni_careplan/models/ni_careplan_category.py
@@ -1,5 +1,5 @@
 #  Copyright (c) 2024 NSTDA
-from odoo import fields, models
+from odoo import _, api, fields, models
 
 
 class CareplanCategory(models.Model):
@@ -7,6 +7,21 @@ class CareplanCategory(models.Model):
     _description = "Careplan Category"
     _inherit = ["ni.coding"]
 
-    condition_code_ids = fields.Many2many("ni.condition.code")
-    goal_category_id = fields.Many2one("ni.goal.category")
-    service_category_id = fields.Many2one("ni.service.category")
+    _parent_store = True
+
+    parent_id = fields.Many2one("ni.careplan.category", index=True, ondelete="set null")
+    parent_path = fields.Char(index=True, unaccent=False)
+
+    condition_code_ids = fields.Many2many(
+        "ni.condition.code", "ni_careplan_category_condition_code"
+    )
+    goal_category_id = fields.Many2one("ni.goal.category", help="Default goal category")
+    goal_code_ids = fields.Many2many("ni.goal.code", "ni_careplan_category_goal_code")
+    service_category_id = fields.Many2one(
+        "ni.service.category", help="Default service category"
+    )
+
+    @api.constrains("parent_id")
+    def _check_parent_id(self):
+        if not self._check_recursion():
+            raise models.ValidationError(_("Error! You cannot create recursive data."))
