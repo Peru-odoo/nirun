186440fdfe70c14bd126823a7149a5348c7f3004
Piruin P. <piruin.p@gmail.com>
2024-11-12 11:16:42 +0700

    [FIX] ni_patient: company show only Identifier

--- ni_patient/models/res_company.py
+++ ni_patient/models/res_company.py
@@ -6,6 +6,8 @@ from odoo import fields, models
 class Company(models.Model):
     _name = "res.company"
     _inherit = ["res.company", "ni.identifier.mixin"]
+    _rec_name = "name"
+    _rec_names_search = "name,identifier,vat"
 
     encounter_class_id = fields.Many2one(
         "ni.encounter.class", help="Default encounter class of this company"
