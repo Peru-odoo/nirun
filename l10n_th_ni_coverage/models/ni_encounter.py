011b3567cbdabc38dc4a41a018eb3dbbc16a8623
Piruin P. <piruin.p@gmail.com>
2024-11-14 14:18:34 +0700

    [FIX] ni_coverage,l10n_th_ni_coverage: Error when no coverage

--- l10n_th_ni_coverage/models/ni_encounter.py
+++ l10n_th_ni_coverage/models/ni_encounter.py
@@ -8,20 +8,20 @@ class Encounter(models.Model):
 
     coverage_type_id = fields.Many2one(
         "ni.coverage.type",
-        "Coverage",
-        domain="[('parent_id', '!=', False)]",
-        compute="_compute_coverage_type_ids",
+        "Use Coverage",
         readonly=False,
         store=True,
-    )
-    coverage_type_parent_id = fields.Many2one(
-        related="coverage_type_id.parent_id", string="Coverage Group"
+        domain="[('id', 'in', coverage_type_ids)]",
     )
 
-    @api.depends("coverage_type_ids")
-    def _compute_coverage_type_ids(self):
+    @api.onchange("patient_id", "coverage_type_ids")
+    def _onchange_patient_coverage(self):
         for rec in self:
-            if rec.coverage_type_ids:
+            if not rec.coverage_type_ids:
+                rec.coverage_type_id = None
+                continue
+            if (
+                not rec.coverage_type_id
+                or rec.coverage_type_id.id not in rec.coverage_type_ids.ids
+            ):
                 rec.coverage_type_id = rec.coverage_type_ids[0]
-            else:
-                rec.coverage_type_ids = False
